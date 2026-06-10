ff3056bc19f76533517660bd6e94da9d88f8961f
abus <99622877+Abus0000@users.noreply.github.com>
2026-06-11 00:02:39 +0200

    Zmiana modelu danych z Produktu na Zadanie, dodanie walidacji i filtrowania

--- desktop/aplikacja_desktop/Form1.Designer.cs
+++ desktop/aplikacja_desktop/Form1.Designer.cs
@@ -22,8 +22,14 @@
             this.lblId = new System.Windows.Forms.Label();
             this.txtNazwa = new System.Windows.Forms.TextBox();
             this.lblNazwa = new System.Windows.Forms.Label();
-            this.txtCena = new System.Windows.Forms.TextBox();
-            this.lblCena = new System.Windows.Forms.Label();
+            this.txtKategoria = new System.Windows.Forms.TextBox();
+            this.lblKategoria = new System.Windows.Forms.Label();
+            this.cmbPriorytet = new System.Windows.Forms.ComboBox();
+            this.lblPriorytet = new System.Windows.Forms.Label();
+            this.dtpTermin = new System.Windows.Forms.DateTimePicker();
+            this.lblTermin = new System.Windows.Forms.Label();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.lblStatus = new System.Windows.Forms.Label();
             this.btnDodaj = new System.Windows.Forms.Button();
             this.btnEdytuj = new System.Windows.Forms.Button();
             this.btnUsun = new System.Windows.Forms.Button();
@@ -44,13 +50,13 @@
             this.dgvData.Name = "dgvData";
             this.dgvData.ReadOnly = true;
             this.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvData.Size = new System.Drawing.Size(460, 380);
+            this.dgvData.Size = new System.Drawing.Size(580, 380);
             this.dgvData.TabIndex = 0;
             this.dgvData.SelectionChanged += new System.EventHandler(this.dgvData_SelectionChanged);
             // 
             // txtId
             // 
-            this.txtId.Location = new System.Drawing.Point(500, 70);
+            this.txtId.Location = new System.Drawing.Point(610, 50);
             this.txtId.Name = "txtId";
             this.txtId.ReadOnly = true;
             this.txtId.Size = new System.Drawing.Size(260, 23);
@@ -59,14 +65,14 @@
             // lblId
             // 
             this.lblId.AutoSize = true;
-            this.lblId.Location = new System.Drawing.Point(500, 52);
+            this.lblId.Location = new System.Drawing.Point(610, 32);
             this.lblId.Name = "lblId";
             this.lblId.Size = new System.Drawing.Size(21, 15);
             this.lblId.Text = "ID:";
             // 
             // txtNazwa
             // 
-            this.txtNazwa.Location = new System.Drawing.Point(500, 120);
+            this.txtNazwa.Location = new System.Drawing.Point(610, 100);
             this.txtNazwa.Name = "txtNazwa";
             this.txtNazwa.Size = new System.Drawing.Size(260, 23);
             this.txtNazwa.TabIndex = 2;
@@ -74,50 +80,98 @@
             // lblNazwa
             // 
             this.lblNazwa.AutoSize = true;
-            this.lblNazwa.Location = new System.Drawing.Point(500, 102);
+            this.lblNazwa.Location = new System.Drawing.Point(610, 82);
             this.lblNazwa.Name = "lblNazwa";
-            this.lblNazwa.Size = new System.Drawing.Size(99, 15);
-            this.lblNazwa.Text = "Nazwa produktu:";
+            this.lblNazwa.Size = new System.Drawing.Size(87, 15);
+            this.lblNazwa.Text = "Nazwa zadania:";
             // 
-            // txtCena
+            // txtKategoria
             // 
-            this.txtCena.Location = new System.Drawing.Point(500, 170);
-            this.txtCena.Name = "txtCena";
-            this.txtCena.Size = new System.Drawing.Size(260, 23);
-            this.txtCena.TabIndex = 3;
+            this.txtKategoria.Location = new System.Drawing.Point(610, 150);
+            this.txtKategoria.Name = "txtKategoria";
+            this.txtKategoria.Size = new System.Drawing.Size(260, 23);
+            this.txtKategoria.TabIndex = 3;
             // 
-            // lblCena
+            // lblKategoria
             // 
-            this.lblCena.AutoSize = true;
-            this.lblCena.Location = new System.Drawing.Point(500, 152);
-            this.lblCena.Name = "lblCena";
-            this.lblCena.Size = new System.Drawing.Size(37, 15);
-            this.lblCena.Text = "Cena:";
+            this.lblKategoria.AutoSize = true;
+            this.lblKategoria.Location = new System.Drawing.Point(610, 132);
+            this.lblKategoria.Name = "lblKategoria";
+            this.lblKategoria.Size = new System.Drawing.Size(60, 15);
+            this.lblKategoria.Text = "Kategoria:";
+            // 
+            // cmbPriorytet
+            // 
+            this.cmbPriorytet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPriorytet.Location = new System.Drawing.Point(610, 200);
+            this.cmbPriorytet.Name = "cmbPriorytet";
+            this.cmbPriorytet.Size = new System.Drawing.Size(260, 23);
+            this.cmbPriorytet.TabIndex = 4;
+            // 
+            // lblPriorytet
+            // 
+            this.lblPriorytet.AutoSize = true;
+            this.lblPriorytet.Location = new System.Drawing.Point(610, 182);
+            this.lblPriorytet.Name = "lblPriorytet";
+            this.lblPriorytet.Size = new System.Drawing.Size(54, 15);
+            this.lblPriorytet.Text = "Priorytet:";
+            // 
+            // dtpTermin
+            // 
+            this.dtpTermin.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpTermin.Location = new System.Drawing.Point(610, 250);
+            this.dtpTermin.Name = "dtpTermin";
+            this.dtpTermin.Size = new System.Drawing.Size(260, 23);
+            this.dtpTermin.TabIndex = 5;
+            // 
+            // lblTermin
+            // 
+            this.lblTermin.AutoSize = true;
+            this.lblTermin.Location = new System.Drawing.Point(610, 232);
+            this.lblTermin.Name = "lblTermin";
+            this.lblTermin.Size = new System.Drawing.Size(102, 15);
+            this.lblTermin.Text = "Termin realizacji:";
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStatus.Location = new System.Drawing.Point(610, 300);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(260, 23);
+            this.cmbStatus.TabIndex = 6;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(610, 282);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(42, 15);
+            this.lblStatus.Text = "Status:";
             // 
             // btnDodaj
             // 
-            this.btnDodaj.Location = new System.Drawing.Point(500, 240);
+            this.btnDodaj.Location = new System.Drawing.Point(610, 355);
             this.btnDodaj.Name = "btnDodaj";
             this.btnDodaj.Size = new System.Drawing.Size(80, 30);
-            this.btnDodaj.TabIndex = 4;
+            this.btnDodaj.TabIndex = 7;
             this.btnDodaj.Text = "Dodaj";
             this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
             // 
             // btnEdytuj
             // 
-            this.btnEdytuj.Location = new System.Drawing.Point(590, 240);
+            this.btnEdytuj.Location = new System.Drawing.Point(700, 355);
             this.btnEdytuj.Name = "btnEdytuj";
             this.btnEdytuj.Size = new System.Drawing.Size(80, 30);
-            this.btnEdytuj.TabIndex = 5;
+            this.btnEdytuj.TabIndex = 8;
             this.btnEdytuj.Text = "Edytuj";
             this.btnEdytuj.Click += new System.EventHandler(this.btnEdytuj_Click);
             // 
             // btnUsun
             // 
-            this.btnUsun.Location = new System.Drawing.Point(680, 240);
+            this.btnUsun.Location = new System.Drawing.Point(790, 355);
             this.btnUsun.Name = "btnUsun";
             this.btnUsun.Size = new System.Drawing.Size(80, 30);
-            this.btnUsun.TabIndex = 6;
+            this.btnUsun.TabIndex = 9;
             this.btnUsun.Text = "Usuń";
             this.btnUsun.Click += new System.EventHandler(this.btnUsun_Click);
             // 
@@ -126,7 +180,7 @@
             this.txtSzukaj.Location = new System.Drawing.Point(12, 21);
             this.txtSzukaj.Name = "txtSzukaj";
             this.txtSzukaj.Size = new System.Drawing.Size(300, 23);
-            this.txtSzukaj.TabIndex = 7;
+            this.txtSzukaj.TabIndex = 10;
             this.txtSzukaj.TextChanged += new System.EventHandler(this.txtSzukaj_TextChanged);
             // 
             // lblSzukaj
@@ -134,17 +188,17 @@
             this.lblSzukaj.AutoSize = true;
             this.lblSzukaj.Location = new System.Drawing.Point(12, 3);
             this.lblSzukaj.Name = "lblSzukaj";
-            this.lblSzukaj.Size = new System.Drawing.Size(103, 15);
-            this.lblSzukaj.Text = "Wyszukaj (Nazwa):";
+            this.lblSzukaj.Size = new System.Drawing.Size(154, 15);
+            this.lblSzukaj.Text = "Wyszukaj (Nazwa/Kategoria):";
             // 
             // btnRaport
             // 
             this.btnRaport.BackColor = System.Drawing.Color.LightGreen;
-            this.btnRaport.Location = new System.Drawing.Point(500, 400);
+            this.btnRaport.Location = new System.Drawing.Point(610, 400);
             this.btnRaport.Name = "btnRaport";
             this.btnRaport.Size = new System.Drawing.Size(260, 30);
-            this.btnRaport.TabIndex = 8;
-            this.btnRaport.Text = "Generuj raport CSV";
+            this.btnRaport.TabIndex = 11;
+            this.btnRaport.Text = "Generuj raport zadań CSV";
             this.btnRaport.UseVisualStyleBackColor = false;
             this.btnRaport.Click += new System.EventHandler(this.btnRaport_Click);
             // 
@@ -152,7 +206,7 @@
             // 
             this.lblError.AutoSize = true;
             this.lblError.ForeColor = System.Drawing.Color.Red;
-            this.lblError.Location = new System.Drawing.Point(500, 205);
+            this.lblError.Location = new System.Drawing.Point(610, 330);
             this.lblError.Name = "lblError";
             this.lblError.Size = new System.Drawing.Size(0, 15);
             // 
@@ -160,7 +214,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 442);
+            this.ClientSize = new System.Drawing.Size(894, 442);
             this.Controls.Add(this.lblError);
             this.Controls.Add(this.btnRaport);
             this.Controls.Add(this.lblSzukaj);
@@ -168,8 +222,14 @@
             this.Controls.Add(this.btnUsun);
             this.Controls.Add(this.btnEdytuj);
             this.Controls.Add(this.btnDodaj);
-            this.Controls.Add(this.lblCena);
-            this.Controls.Add(this.txtCena);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.lblTermin);
+            this.Controls.Add(this.dtpTermin);
+            this.Controls.Add(this.lblPriorytet);
+            this.Controls.Add(this.cmbPriorytet);
+            this.Controls.Add(this.lblKategoria);
+            this.Controls.Add(this.txtKategoria);
             this.Controls.Add(this.lblNazwa);
             this.Controls.Add(this.txtNazwa);
             this.Controls.Add(this.lblId);
@@ -178,7 +238,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "System Zarządzania Produktami";
+            this.Text = "System Zarządzania Zadaniami (To-Do)";
             ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -191,8 +251,14 @@
         private System.Windows.Forms.Label lblId;
         private System.Windows.Forms.TextBox txtNazwa;
         private System.Windows.Forms.Label lblNazwa;
-        private System.Windows.Forms.TextBox txtCena;
-        private System.Windows.Forms.Label lblCena;
+        private System.Windows.Forms.TextBox txtKategoria;
+        private System.Windows.Forms.Label lblKategoria;
+        private System.Windows.Forms.ComboBox cmbPriorytet;
+        private System.Windows.Forms.Label lblPriorytet;
+        private System.Windows.Forms.DateTimePicker dtpTermin;
+        private System.Windows.Forms.Label lblTermin;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.Button btnDodaj;
         private System.Windows.Forms.Button btnEdytuj;
         private System.Windows.Forms.Button btnUsun;
--- desktop/aplikacja_desktop/Form1.cs
+++ desktop/aplikacja_desktop/Form1.cs
@@ -7,90 +7,124 @@ using System.Windows.Forms;
 
 namespace aplikacja_desktop
 {
-    // Definicja modelu danych
-    public class Produkt
+    // Definicja modelu danych z obsługą właściwości dopuszczających null
+    public class Zadanie
     {
         public int Id { get; set; }
-        public string Nazwa { get; set; }
-        public decimal Cena { get; set; }
+        public string Nazwa { get; set; } = string.Empty;
+        public string Kategoria { get; set; } = string.Empty;
+        public string Priorytet { get; set; } = string.Empty;
+        public DateTime Termin { get; set; }
+        public string Status { get; set; } = string.Empty; // Nowe / W trakcie / Wykonane
     }
 
     public partial class Form1 : Form
     {
-        // BindingList pozwala na automatyczne odświeżanie tabeli DataGridView
-        private BindingList<Produkt> listaProduktow;
-        private BindingSource bindingSource;
+        // Inicjalizacja pól, aby uniknąć ostrzeżeń CS8618
+        private BindingList<Zadanie> listaZadan = new BindingList<Zadanie>();
+        private BindingSource bindingSource = new BindingSource();
         private int nastepneId = 4;
 
         public Form1()
         {
             InitializeComponent();
+            InicjalizujKomponentyWlasne();
             InicjalizujDane();
+        }
+
+        // Konfiguracja ComboBoxów (wartości słownikowe)
+        private void InicjalizujKomponentyWlasne()
+        {
+            cmbPriorytet.Items.AddRange(new string[] { "Niski", "Średni", "Wysoki" });
+            cmbStatus.Items.AddRange(new string[] { "Nowe", "W trakcie", "Wykonane" });
+            
+            cmbPriorytet.SelectedIndex = 1; // Średni
+            cmbStatus.SelectedIndex = 0;    // Nowe
         }
 
         private void InicjalizujDane()
         {
-            // Przykładowe dane początkowe
-            listaProduktow = new BindingList<Produkt>()
+            // Przykładowe dane początkowe dla zadań
+            listaZadan = new BindingList<Zadanie>()
             {
-                new Produkt { Id = 1, Nazwa = "Laptop", Cena = 3499.99m },
-                new Produkt { Id = 2, Nazwa = "Mysz bezprzewodowa", Cena = 120.00m },
-                new Produkt { Id = 3, Nazwa = "Monitor 4K", Cena = 1550.50m }
+                new Zadanie { Id = 1, Nazwa = "Zaimplementować CRUD", Kategoria = "Programowanie", Priorytet = "Wysoki", Termin = DateTime.Now.AddDays(2), Status = "W trakcie" },
+                new Zadanie { Id = 2, Nazwa = "Przygotować raport końcowy", Kategoria = "Dokumentacja", Priorytet = "Średni", Termin = DateTime.Now.AddDays(5), Status = "Nowe" },
+                new Zadanie { Id = 3, Nazwa = "Przetestować aplikację", Kategoria = "Testy", Priorytet = "Niski", Termin = DateTime.Now.AddDays(7), Status = "Wykonane" }
             };
 
-            bindingSource = new BindingSource { DataSource = listaProduktow };
+            bindingSource.DataSource = listaZadan;
             dgvData.DataSource = bindingSource;
 
-            // Konfiguracja kolumn i włączenie wbudowanego sortowania w DataGridView
-            dgvData.Columns["Id"].Width = 50;
-            dgvData.Columns["Nazwa"].Width = 240;
-            dgvData.Columns["Cena"].Width = 120;
-            dgvData.Columns["Cena"].DefaultCellStyle.Format = "C2"; // Format waluty
+            // Konfiguracja kolumn DataGridView
+            dgvData.Columns["Id"].Width = 40;
+            dgvData.Columns["Nazwa"].Width = 150;
+            dgvData.Columns["Kategoria"].Width = 100;
+            dgvData.Columns["Priorytet"].Width = 80;
+            dgvData.Columns["Termin"].Width = 110;
+            dgvData.Columns["Status"].Width = 90;
+            
+            dgvData.Columns["Termin"].DefaultCellStyle.Format = "yyyy-MM-dd";
         }
 
-        // WALIDACJA FORMULARZA
-        private bool WalidujFormularz(out string nazwa, out decimal cena)
+        // WALIDACJA DANYCH
+        private bool WalidujFormularz(out string nazwa, out string kategoria, out string priorytet, out DateTime termin, out string status)
         {
             nazwa = txtNazwa.Text.Trim();
-            cena = 0;
+            kategoria = txtKategoria.Text.Trim();
+            priorytet = cmbPriorytet.SelectedItem?.ToString() ?? "Średni";
+            termin = dtpTermin.Value;
+            status = cmbStatus.SelectedItem?.ToString() ?? "Nowe";
+            
             lblError.Text = "";
 
             if (string.IsNullOrEmpty(nazwa))
             {
-                lblError.Text = "Błąd: Nazwa nie może być pusta!";
+                lblError.Text = "Błąd: Nazwa zadania nie może być pusta!";
                 return false;
             }
 
-            if (!decimal.TryParse(txtCena.Text, out cena) || cena <= 0)
+            if (string.IsNullOrEmpty(kategoria))
             {
-                lblError.Text = "Błąd: Cena musi być liczbą większą od 0!";
+                lblError.Text = "Błąd: Kategoria nie może być pusta!";
                 return false;
             }
 
             return true;
         }
 
-        // CRUD: Odczyt - przeniesienie danych z tabeli do pól tekstowych przy kliknięciu w wiersz
+        // CRUD: Odczyt (Zaznaczenie wiersza przenosi dane do pól edycji)
         private void dgvData_SelectionChanged(object sender, EventArgs e)
         {
             if (dgvData.SelectedRows.Count > 0)
             {
-                var produkt = (Produkt)dgvData.SelectedRows[0].DataBoundItem;
-                txtId.Text = produkt.Id.ToString();
-                txtNazwa.Text = produkt.Nazwa;
-                txtCena.Text = produkt.Cena.ToString();
+                var zadanie = (Zadanie)dgvData.SelectedRows[0].DataBoundItem;
+                txtId.Text = zadanie.Id.ToString();
+                txtNazwa.Text = zadanie.Nazwa;
+                txtKategoria.Text = zadanie.Kategoria;
+                cmbPriorytet.SelectedItem = zadanie.Priorytet;
+                dtpTermin.Value = zadanie.Termin;
+                cmbStatus.SelectedItem = zadanie.Status;
             }
         }
 
         // CRUD: Dodawanie
         private void btnDodaj_Click(object sender, EventArgs e)
         {
-            if (WalidujFormularz(out string nazwa, out decimal cena))
+            if (WalidujFormularz(out string nazwa, out string kategoria, out string priorytet, out DateTime termin, out string status))
             {
-                var nowy = new Produkt { Id = nastepneId++, Nazwa = nazwa, Cena = cena };
-                listaProduktow.Add(nowy);
+                // TUTAJ poprawiłem literówkę z "nowieZadanie" na "noweZadanie"
+                var noweZadanie = new Zadanie 
+                { 
+                    Id = nastepneId++, 
+                    Nazwa = nazwa, 
+                    Kategoria = kategoria, 
+                    Priorytet = priorytet, 
+                    Termin = termin, 
+                    Status = status 
+                };
+                listaZadan.Add(noweZadanie);
                 WyczyscPola();
-                MessageBox.Show("Produkt dodany pomyślnie!", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Zadanie dodane pomyślnie!", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
@@ -99,19 +133,22 @@ namespace aplikacja_desktop
         {
             if (dgvData.SelectedRows.Count > 0)
             {
-                if (WalidujFormularz(out string nazwa, out decimal cena))
+                if (WalidujFormularz(out string nazwa, out string kategoria, out string priorytet, out DateTime termin, out string status))
                 {
-                    var produkt = (Produkt)dgvData.SelectedRows[0].DataBoundItem;
-                    produkt.Nazwa = nazwa;
-                    produkt.Cena = cena;
+                    var zadanie = (Zadanie)dgvData.SelectedRows[0].DataBoundItem;
+                    zadanie.Nazwa = nazwa;
+                    zadanie.Kategoria = kategoria;
+                    zadanie.Priorytet = priorytet;
+                    zadanie.Termin = termin;
+                    zadanie.Status = status;
 
-                    listaProduktow.ResetBindings(); // Odświeżenie widoku tabeli
-                    MessageBox.Show("Produkt zaktualizowany!", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    listaZadan.ResetBindings(); // Odświeżenie widoku tabeli
+                    MessageBox.Show("Zadanie zaktualizowane!", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
             else
             {
-                MessageBox.Show("Wybierz produkt z tabeli do edycji.", "Uwaga", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Wybierz zadanie z tabeli do edycji.", "Uwaga", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
@@ -120,22 +157,22 @@ namespace aplikacja_desktop
         {
             if (dgvData.SelectedRows.Count > 0)
             {
-                var produkt = (Produkt)dgvData.SelectedRows[0].DataBoundItem;
+                var zadanie = (Zadanie)dgvData.SelectedRows[0].DataBoundItem;
 
-                var wynik = MessageBox.Show($"Czy na pewno chcesz usunąć: {produkt.Nazwa}?", "Potwierdzenie", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                var wynik = MessageBox.Show($"Czy na pewno chcesz usunąć zadanie: {zadanie.Nazwa}?", "Potwierdzenie", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (wynik == DialogResult.Yes)
                 {
-                    listaProduktow.Remove(produkt);
+                    listaZadan.Remove(zadanie);
                     WyczyscPola();
                 }
             }
             else
             {
-                MessageBox.Show("Wybierz produkt z tabeli do usunięcia.", "Uwaga", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Wybierz zadanie z tabeli do usunięcia.", "Uwaga", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
-        // WYSZUKIWANIE / FILTROWANIE dynamiczne
+        // WYSZUKIWANIE I FILTROWANIE dynamiczne po nazwie oraz kategorii
         private void txtSzukaj_TextChanged(object sender, EventArgs e)
         {
             string fraza = txtSzukaj.Text.ToLower();
@@ -145,37 +182,40 @@ namespace aplikacja_desktop
             }
             else
             {
-                var przefiltrowane = listaProduktow.Where(p => p.Nazwa.ToLower().Contains(fraza)).ToList();
+                var przefiltrowane = listaZadan.Where(z => 
+                    z.Nazwa.ToLower().Contains(fraza) || 
+                    z.Kategoria.ToLower().Contains(fraza)
+                ).ToList();
                 dgvData.DataSource = new BindingSource { DataSource = przefiltrowane };
             }
         }
 
-        // GENEROWANIE RAPORTU CSV
+        // GENEROWANIE RAPORTU CSV (Uwzględnia filtry w DataGridView)
         private void btnRaport_Click(object sender, EventArgs e)
         {
             using (SaveFileDialog sfd = new SaveFileDialog())
             {
                 sfd.Filter = "Pliki CSV (*.csv)|*.csv";
-                sfd.FileName = $"Raport_Produktow_{DateTime.Now:yyyyMMdd_HHmmss}.csv";
+                sfd.FileName = $"Raport_Zadan_{DateTime.Now:yyyyMMdd_HHmmss}.csv";
 
                 if (sfd.ShowDialog() == DialogResult.OK)
                 {
                     try
                     {
                         StringBuilder sb = new StringBuilder();
-                        // Nagłówki kolumn
-                        sb.AppendLine("ID;Nazwa Produktu;Cena");
+                        sb.AppendLine("ID;Nazwa Zadania;Kategoria;Priorytet;Termin;Status");
 
-                        // Zawartość aktualnie widoczna w DataGridView (uwzględnia filtry)
                         foreach (DataGridViewRow row in dgvData.Rows)
                         {
-                            var prod = (Produkt)row.DataBoundItem;
-                            sb.AppendLine($"{prod.Id};{prod.Nazwa};{prod.Cena}");
+                            var zad = (Zadanie)row.DataBoundItem;
+                            if (zad != null)
+                            {
+                                sb.AppendLine($"{zad.Id};{zad.Nazwa};{zad.Kategoria};{zad.Priorytet};{zad.Termin:yyyy-MM-dd};{zad.Status}");
+                            }
                         }
 
-                        // Zapis z kodowaniem UTF8 dla zachowania polskich znaków
                         File.WriteAllText(sfd.FileName, sb.ToString(), Encoding.UTF8);
-                        MessageBox.Show("Raport został pomyślnie wygenerowany!", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        MessageBox.Show("Raport zadań został pomyślnie wygenerowany!", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
                     catch (Exception ex)
                     {
@@ -189,7 +229,10 @@ namespace aplikacja_desktop
         {
             txtId.Clear();
             txtNazwa.Clear();
-            txtCena.Clear();
+            txtKategoria.Clear();
+            cmbPriorytet.SelectedIndex = 1;
+            dtpTermin.Value = DateTime.Now;
+            cmbStatus.SelectedIndex = 0;
             lblError.Text = "";
         }
     }
